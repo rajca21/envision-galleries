8673e95a35353d05e63538f40515bc24d5f3cca6
rajca21 <2nikolar1@gmail.com>
2024-10-16 16:00:37 +0200

    try catch for artsy

--- client/src/utils/artsyApi.js
+++ client/src/utils/artsyApi.js
@@ -1,26 +1,35 @@
 import axios from 'axios';
 
 const getAuthToken = async () => {
-  const res = await axios.post(
-    `https://api.artsy.net/api/tokens/xapp_token?client_id=${
-      import.meta.env.VITE_ARTSY_CLIENT_ID
-    }&client_secret=${import.meta.env.VITE_ARTSY_CLIENT_SECRET}`,
-    {},
-    {
-      withCredentials: true,
-    }
-  );
+  try {
+    const res = await axios.post(
+      `https://api.artsy.net/api/tokens/xapp_token?client_id=${
+        import.meta.env.VITE_ARTSY_CLIENT_ID
+      }&client_secret=${import.meta.env.VITE_ARTSY_CLIENT_SECRET}`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
 
-  return res.data.token;
+    return res.data.token;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export const getArtworks = async () => {
-  const token = await getAuthToken();
-  const res = await axios.get('https://api.artsy.net/api/artworks', {
-    headers: {
-      'X-Xapp-Token': token,
-    },
-    withCredentials: true,
-  });
-  return res.data._embedded.artworks;
+  try {
+    const token = await getAuthToken();
+    const res = await axios.get('https://api.artsy.net/api/artworks', {
+      headers: {
+        'X-Xapp-Token': token,
+      },
+      withCredentials: true,
+    });
+    return res.data._embedded.artworks;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
