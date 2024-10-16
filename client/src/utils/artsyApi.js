131934722c050cd8f77f85d610f435ae4d06e97a
rajca21 <2nikolar1@gmail.com>
2024-10-16 15:57:00 +0200

    enabling credentials for Artsy API

--- client/src/utils/artsyApi.js
+++ client/src/utils/artsyApi.js
@@ -4,7 +4,11 @@ const getAuthToken = async () => {
   const res = await axios.post(
     `https://api.artsy.net/api/tokens/xapp_token?client_id=${
       import.meta.env.VITE_ARTSY_CLIENT_ID
-    }&client_secret=${import.meta.env.VITE_ARTSY_CLIENT_SECRET}`
+    }&client_secret=${import.meta.env.VITE_ARTSY_CLIENT_SECRET}`,
+    {},
+    {
+      withCredentials: true,
+    }
   );
 
   return res.data.token;
@@ -16,6 +20,7 @@ export const getArtworks = async () => {
     headers: {
       'X-Xapp-Token': token,
     },
+    withCredentials: true,
   });
   return res.data._embedded.artworks;
 };
